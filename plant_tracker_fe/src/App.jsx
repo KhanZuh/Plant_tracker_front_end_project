2a523ed7b8beb606d2b3200561f4bca2e44c5786
Thomas Peace <thomasjpeace29@gmail.com>
2024-07-08 13:58:52 +0100

    create state variables and fetch requests

--- plant_tracker_fe/src/App.jsx
+++ plant_tracker_fe/src/App.jsx
@@ -2,8 +2,47 @@ import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css'
 import { Navbar } from "react-bootstrap"
 import Home from "./Components/Home"
+import { useState, useEffect } from "react"
+
 
 function App() {
+
+  const [users, setUsers] = useState([])
+  const [plants, setPlants] = useState([])
+  const [duties, setDuties] = useState([])
+  const [countries, setCountries] = useState([])
+
+  const fetchUsers = async() => {
+    const response = await fetch('http://localhost:8080/people');
+    const data = await response.json();
+    setUsers(data);
+  }
+
+  const fetchPlants = async() => {
+    const response = await fetch('http://localhost:8080/plants');
+    const data = await response.json();
+    setPlants(data);
+  }
+
+  const fetchDuties = async() => {
+    const response = await fetch('http://localhost:8080/duties');
+    const data = await response.json();
+    setDuties(data);
+  }
+
+  const fetchCountries = async() => {
+    const response = await fetch('http://localhost:8080/countries');
+    const data = await response.json();
+    setCountries(data);
+  }
+
+  useEffect(() => {
+    fetchUsers(),
+    fetchPlants(),
+    fetchDuties(),
+    fetchCountries()}, [])
+
+
 
   return (
     <Router>
