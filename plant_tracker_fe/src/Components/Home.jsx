3e05954d14e3e5338ecdfdd507c0d184f3d947d8
KhanZuh <khanzuhair96@live.co.uk>
2024-07-08 13:33:12 +0100

    Home.jsx

---
+++ plant_tracker_fe/src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import {Link} from "react-router-dom"
+
+const Home = () => {
+
+    return(
+        <div className="Home">
+            <h1>Plant Tracker</h1>
+            <p>Track, nurture and watch your garden flourish </p>
+        </div>
+    )
+}
+
+export default Home;
